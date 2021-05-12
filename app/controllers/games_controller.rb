b23afda1af19005ca0725b2314fee8e1476dae6f
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-12 13:07:25 +0200

    implement score part

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,33 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
 
   def new
-    @letters
+    @letters = []
+    10.times { @letters << ('A'..'Z').to_a.sample}
   end
 
   def score
-    @score
+    try = params[:word].chars
+    grid = params[:letters]
+    if is_english_word? && compare_words(try, grid)
+      return @result = "yeah!"
+    end
+  end
+
+
+private
+
+  def is_english_word?
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    player_guess_serialized = URI.open(url).read
+    player_guess = JSON.parse(player_guess_serialized)
+    return player_guess["found"]
+  end
+
+  def compare_words(try,grid)
+    try.all? {|letter| grid.count(letter) >= try.count(letter)}
   end
 end
+
