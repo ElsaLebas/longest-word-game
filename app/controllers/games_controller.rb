d1191245b7edaaeb694e1fd66a77a4b068475b6b
Elsa Lebas <lebas.elsa@gmail.com>
2021-05-13 10:10:39 +0200

    implement conditions on result game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,32 +2,32 @@ require 'json'
 require 'open-uri'
 
 class GamesController < ApplicationController
-
   def new
     @letters = []
     10.times { @letters << ('A'..'Z').to_a.sample}
   end
 
   def score
-    try = params[:word].chars
+    try = params[:word].upcase.chars
     grid = params[:letters]
-    if is_english_word? && compare_words(try, grid)
-      return @result = "yeah!"
+    if !word_letters_in_grid?(try, grid)
+      @result = "Sorry but #{params[:word].upcase}, can't be built out of #{grid}"
+    elsif !english_word?
+      @result = "Sorry but #{params[:word].upcase}, is not a valid English word ..."
+    else
+      @result = "Congratulations! #{params[:word].upcase} is a valid English word"
     end
   end
 
-
-private
-
-  def is_english_word?
+  private
+  def english_word?
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     player_guess_serialized = URI.open(url).read
     player_guess = JSON.parse(player_guess_serialized)
-    return player_guess["found"]
+    player_guess["found"]
   end
 
-  def compare_words(try,grid)
-    try.all? {|letter| grid.count(letter) >= try.count(letter)}
+  def word_letters_in_grid?(try, grid)
+    try.all? { |letter| grid.count(letter) >= try.count(letter) }
   end
 end
-
